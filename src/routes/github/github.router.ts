0389ca33de205dcf5d176ed50e96e3d68ba49278
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2019-04-17 21:01:42 +0200

    feature: isAuthority value in JWT

--- src/routes/github/github.router.ts
+++ src/routes/github/github.router.ts
@@ -7,7 +7,9 @@
 import configServer from '@config/server.config';
 import { redisSetTokenIntoStorage } from '@utils/redis.util';
 import { validateTokenTemporary } from '@utils/validator.util';
-import { getManager } from '@utils/typeorm.util';
+import { getConnection, getManager } from '@utils/typeorm.util';
+import { getRepository } from 'typeorm';
+import { UsersModel } from '@modelsSQL/Users.model';
 
 const express = require('express');
 const router = express.Router();
@@ -71,24 +73,37 @@ async function githubCallback(req, res) {
    // Destroy session
    req.session = null;
 
+   // Get user permissions
    const permissionsView: object[] = await getManager().query(
       `SELECT * FROM view_user_permissions WHERE user_id=${req.user.id}`,
    );
-
    let permissionsPure: string[] = [];
-
    permissionsView.forEach((item: object) => {
       permissionsPure.push(item['permission_namespace'] + '.' + item['key_namespace']);
    });
+
+   // Get user authority
+   const connection = getConnection();
+   const repoUsers = connection.getRepository(UsersModel);
+   let user: UsersModel, isAuthority;
+   try {
+      user = await repoUsers.findOneOrFail({
+         where: { id: req.user.id },
+         relations: ['role'],
+      });
+      isAuthority = user.role.isAuthority;
+   } catch (e) {}
 
    // Validate temporary token
    if (!validateTokenTemporary(tokenTmp)) return res.send('Sorry, no temporary token provided.');
 
    // Create permanent token and save it to db
-   redisSetTokenIntoStorage(tokenTmp, userID, authID, authType, permissionsPure).then(() => {
-      // Successful authorization
-      res.send('Successful authorization, you can close this window.');
-   });
+   redisSetTokenIntoStorage(tokenTmp, userID, authID, authType, permissionsPure, isAuthority).then(
+      () => {
+         // Successful authorization
+         res.send('Successful authorization, you can close this window.');
+      },
+   );
 }
 
 /**
